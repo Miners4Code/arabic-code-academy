ada75965cac9faa32a09c1ce765515905171cae6
kamelmaher <kamelmahersh@gmail.com>
2024-10-22 16:27:04 +0300

    Finishing-Task-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client"
+import { useEffect, useState } from "react";
+import styles from "./page.module.css"
 export default function Home() {
+  const [show, setShow] = useState(false)
+  const startDate = new Date("2024-10-20")
+  const endDate = new Date("2024-10-25")
+  const currentDate = new Date()
+  useEffect(() => {
+    if (currentDate > startDate && currentDate < endDate) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }, [])
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Hello React Next Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>React Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      {
+        show &&
+        <div
+          style={{
+            background: "#4e40a8",
+            textAlign: "center",
+            padding: "32px",
+            color: "white"
+          }}>
+          <h3 className={styles.heading}>
+            خصومات بنسبة 20% على كل الكورسات
+          </h3>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      }
+
     </div>
   );
 }

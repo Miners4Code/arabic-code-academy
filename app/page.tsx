e00bd56942f0966bd964ed17a04075d2d39eb17a
Omar <omarsahmoud02@gmail.com>
2024-10-22 16:33:23 +0200

    omar-task-1-finishing the discount component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Discount from "@/components/atoms/discount";
+import Discount from "@/components/atoms/Discount";
 
 
 export default function Home() {
--- components/atoms/Discount.tsx
+++ components/atoms/Discount.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-interface Props{
-    text?: string;
-    startingDate?: Date;
-    endingDate?: Date;
+interface Props {
+  text?: string;
+  startingDate?: Date;
+  endingDate?: Date;
 }
+const today = new Date();
+const Discount: React.FC<Props> = ({
+  text = "خصومات بنسبة 20% على الكورسات",
+  startingDate = new Date("2024-10-01"),
+  endingDate = new Date("2024-10-23"),
+}) => {
+  const isInDiscountPeriod = today >= startingDate && today <= endingDate;
 
-const Discount:React.FC<Props> = ({text="hello world"}) => {
-    return (
-        <div style={{padding:"32px",backgroundColor:"#ff0000"}}>
-            {text}
-        </div>
-    );
-}
+  return isInDiscountPeriod ? (
+    <div style={{textAlign:"center",padding:"30px",backgroundColor: "#462576", color: "#fff"}}>
+      {text}
+    </div>
+    
+  ) : null;
+};
 
-export default Discount
+export default Discount;

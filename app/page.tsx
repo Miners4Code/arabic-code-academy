e777385b4c72916a25b416a4f2cd5a89a0b81209
m7mood <mahmoud.azamtta19@gmail.com>
2024-11-04 20:51:20 +0200

    Setup Chakra-UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import DiscountBanner from "../components/DiscountBanner/DiscountBanner";
+import { Box, Button } from "@chakra-ui/react";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <DiscountBanner
-          text={"خصومات بنسبة 20% على الكورسات"}
-          start={new Date("2024-10-19T18:00:00")}
-          end={new Date("2024-10-23T18:00:00")}
-        />
+        <Box>
+          <Button>Hello world</Button>
+        </Box>
       </main>
     </div>
   );

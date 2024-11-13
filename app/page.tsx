c4e46dbe7ea53381602a9303b06ce3339af8ace4
m7mood <mahmoud.azamtta19@gmail.com>
2024-11-13 17:49:13 +0200

    Apply the notes given on task 2 PR

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import ExaminationSection from "@/sections/ExaminationSection";
+import Exams from "@/sections/Examination/Exams";
 import styles from "./page.module.css";
 import EduTracks from "@/sections/EduTracks/EduTracks";
-import Contact from "@/sections/section3/Contact";
+import Contact from "@/sections/Contacts/Contact";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ExaminationSection />
+        <Exams />
         <EduTracks />
         <Contact />
       </main>

0af12467521b2defa6d1edee70be579a7c1946de
m7mood <mahmoud.azamtta19@gmail.com>
2024-11-17 01:40:54 +0200

    Fix section import issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import EduTracks from "@/sections/EduTracks/EduTracks";
 import Contact from "@/sections/Contacts/Contact";
 import Feedback from "@/Card/Feedback-card";
 import Products from "@/sections/Products/Products";
+import Exams from "@/sections/Examination/Exams";
+import Resources from "@/sections/Resources/Resources";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Exams />
+        <Resources />
         <EduTracks />
         <Contact />
         <Products />

494ea042e6461afa19c8c6acd9fead2c0481c4d2
m7mood <mahmoud.azamtta19@gmail.com>
2024-11-29 18:40:48 +0200

    Fix fonts issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
-import ACAButton from "@/components/ACAButton/ACAButton";
-import HeaderSlide from "@/components/HeaderSlide/HeaderSlide";
-import CustomIcon from "@/icons/Chalkboard";
+import Hero from "@/sections/Hero/Hero";
+import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
-  const slideData = {
-    background: "/headerImage1.jpg",
-    description: "تعمل الأكاديمية العربية للبرمجة كجسر يربط العقول التكنولوجية العربية في المهجر بالطلبة العرب أينما كانوا",
-    sub_description1: "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية",
-    sub_description2: " وأسلوب تدريسي ممتع يتناسب مع مختلف الطرق التعليمية للمبتدئين والمحترفين بإشراف مدربين ومبرمجين ذوي خبرة عالمية في المجال التقني"
-  };
-
   return (
-    <HeaderSlide slide_data={slideData}>
-      <ACAButton
-            text="المسارات التعليمية"
-            icon={<CustomIcon />}
-            bg="cyan"
-            weight="900"
-            size="lg"
-          />
-    </HeaderSlide>
+    <Flex as={"main"} flexDir={"column"} gap={"32px"} gridRowStart={2}>
+      <Hero />
+    </Flex>
   );
 }

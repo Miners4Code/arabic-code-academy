3d8355102489306369ebb553462b2a80ea768a86
m7mood <mahmoud.azamtta19@gmail.com>
2024-12-01 01:11:34 +0200

    remove unnecessary white space between sections

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex as={"main"} flexDir={"column"} gap={"32px"} gridRowStart={2}>
+    <Flex as={"main"} flexDir={"column"} gridRowStart={2}>
       <Navbar />
       <Hero />
       <ArabicLang />
